2c51cd75466590a11c4b8eaf2af9c18d57f9f3c1
AldinBegio <66532285+AldinBegio@users.noreply.github.com>
2020-10-11 19:22:33 +0200

    Update settings.py

--- projekt_ip/settings.py
+++ projekt_ip/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(file))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
