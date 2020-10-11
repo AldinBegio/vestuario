688a538d2a5ce2fd2e899d0c5d919ac9156acab0
AldinBegio <66532285+AldinBegio@users.noreply.github.com>
2020-10-11 21:13:13 +0200

    Update settings.py

--- projekt_ip/settings.py
+++ projekt_ip/settings.py
@@ -132,6 +132,7 @@ LOGOUT_REDIRECT_URL = 'login'
 # EMAIL SETTINGS
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST_USER = 'vestuariocompany@gmail.com'
 EMAIL_HOST_PASSWORD = 'Vestuario321'
 EMAIL_USE_TLS = True
